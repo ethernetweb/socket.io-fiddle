ab73709c091657bd2331d5d8d3560c48758fa28a
ethernetweb <public@ethernetweb.com>
2017-02-09 01:14:30 -0500

    Changes

--- client.js
+++ client.js
@@ -1,8 +1,22 @@
+var options = {forceNew:true,timeout:15000,reconnection:true,reconnectionDelay:35000};
+var socket = require('socket.io-client')('http://localhost:3000',options);
 
-const socket = require('socket.io-client')('http://localhost:3000');
+process.stdin.resume();
 
-socket.on('connect', onConnect);
+connInit(socket);
 
-function onConnect(){
-  console.log('connect ' + socket.id);
+function connInit(socket) {
+  socket.on('connect', function(data) {
+    console.log('connect ' + socket.id);
+  });
+
+  socket.on("disconnect", function(data) {
+    console.log("disconnect: "+data);
+    socket.io.reconnect();
+    console.log("skipReconnect:"+socket.io.skipReconnect);
+  });
+
+  socket.on("reconnect", function(data) {
+    console.log("reconnect: "+data);
+  });
 }
--- server.js
+++ server.js
@@ -8,10 +8,19 @@ const port = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/public'));
 
 io.on('connect', onConnect);
+
 server.listen(port, () => console.log('server listening on port ' + port));
 
 function onConnect(socket){
   console.log('connect ' + socket.id);
 
-  socket.on('disconnect', () => console.log('disconnect ' + socket.id));
+
+// modification here
+
+  console.log("now disconnecting...");
+  socket.disconnect();
+
+  socket.on('disconnect', function(data) {
+    console.log("disconnect: "+data);
+  });
 }
